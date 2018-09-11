f97cbd50957330b5a004c96e17cca1facc244c99
Young <youngkwon313@gmail.com>
2018-09-11 23:49:47 +0000

    All javascripting files added

---
+++ if.statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+len_fru = fruit.length
+if (len_fru > 5){
+  console.log("The fruit name has more than five characters.")
+}
+else{
+  console.log("The fruit name has five chracters or less.")
+}
